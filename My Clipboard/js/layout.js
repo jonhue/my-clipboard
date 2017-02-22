a455f007b7ae55d171a256c765c23e7eb8c64394
jonhue <jonas.huebotter@gmx.de>
2017-02-22 10:29:56 +0100

    Minor Improvements & Increase Version

--- My Clipboard/js/layout.js
+++ My Clipboard/js/layout.js
@@ -20,14 +20,14 @@ if (window.jQuery) {
     };
 
     function openPro() {
-        $('section#history, header').stop().animate({ opacity: 0 }, 100);
+        $('section#history, header').stop().animate({ opacity: '0 !important' }, 100);
         $('section#pro .wrapper').stop().fadeOut(100);
         setTimeout(function() {
             $('section#pro').addClass('fullPage opened');
             $('html, body').stop().animate({
                 'scrollTop':  $(document).height()
             }, 750, 'swing');
-            $('section#pro .middle, section#pro .bottom').show();
+            $('section#pro .middle, section#pro .bottom, #pro-close').show();
             $('section#pro .wrapper').stop().fadeIn(350);
         }, 500);
     };
@@ -35,13 +35,13 @@ if (window.jQuery) {
     function closePro() {
         $('section#pro .wrapper').stop().fadeOut(250, function() {
             $('section#pro').removeClass('fullPage');
-            $('section#pro .middle, section#pro .bottom').hide();
+            $('section#pro .middle, section#pro .bottom, #pro-close').hide();
             $('html, body').stop().animate({
                 'scrollTop':  0
             }, 350, 'swing');
             setTimeout(function() {
                 $('section#pro .wrapper').stop().fadeIn(100);
-                $('section#history, header').stop().animate({ opacity: 1 }, 100);
+                $('section#history, header').stop().animate({ opacity: '1 !important' }, 100);
             }, 800);
         });
     };
