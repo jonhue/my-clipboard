56f641cd26a6690e660fd4034dd4cd0f8ec6c1c2
jonhue <jonas.huebotter@gmx.de>
2017-02-26 00:09:51 +0100

    Modify Time Format

--- My Clipboard/js/api/track_history.js
+++ My Clipboard/js/api/track_history.js
@@ -4,21 +4,31 @@ function getDate(seconds) {
     var month = d.getMonth()+1;
     var day = d.getDate();
     var hour = d.getHours();
+    hour = ( hour + 24 ) % 24;
+    var mid='AM';
+    if ( hour == 0 ) {
+        hour = 12;
+    } else if ( hour > 12 ) {
+        hour = hour % 12;
+        mid = 'PM';
+    };
     var minute = d.getMinutes();
 
     if ( seconds ) {
         var second = d.getSeconds();
 
-        var date = ((''+month).length<2 ? '0' : '') + month + '-' +
-            ((''+day).length<2 ? '0' : '') + day + ' ' +
+        var date = ((''+month).length<2 ? '0' : '') + month + '/' +
+            ((''+day).length<2 ? '0' : '') + day + ' - ' +
             ((''+hour).length<2 ? '0' :'') + hour + ':' +
             ((''+minute).length<2 ? '0' :'') + minute + ':' +
-            ((''+second).length<2 ? '0' :'') + second;
+            ((''+second).length<2 ? '0' :'') + second + '  ' +
+            mid
     } else {
-        var date = ((''+month).length<2 ? '0' : '') + month + '-' +
-            ((''+day).length<2 ? '0' : '') + day + ' ' +
+        var date = ((''+month).length<2 ? '0' : '') + month + '/' +
+            ((''+day).length<2 ? '0' : '') + day + ' - ' +
             ((''+hour).length<2 ? '0' :'') + hour + ':' +
-            ((''+minute).length<2 ? '0' :'') + minute
+            ((''+minute).length<2 ? '0' :'') + minute + '  ' +
+            mid
     };
 
     return date
