c01f890c9b05d169debe8c5309c7994e51bd85f9
jonhue <jonas.huebotter@gmx.de>
2017-02-25 21:27:45 +0100

    Modify track_history.js

--- My Clipboard/js/api/track_history.js
+++ My Clipboard/js/api/track_history.js
@@ -6,9 +6,9 @@ function trackHistory() {
 
     // Check for My Clipboard pro
     if (licenseInformation.productLicenses["1"].isActive) {
-        var max = 500;
+        var maxHistoryEvents = 500;
     } else {
-        var max = 5;
+        var maxHistoryEvents = 5;
     };
 
     // get Clipboard if changed to last event
@@ -17,24 +17,29 @@ function trackHistory() {
     if ( text == " " || text == "" ) {
         $('#clipboard-icon').addClass('cleared');
     } else {
-        // Update associated values
+        // Update historyEventsCount
         var roamingSettings = Windows.Storage.ApplicationData.current.roamingSettings;
-        roamingSettings.values["historyEventsCount"] = roamingSettings.values["historyEventsCount"] + 1
-        if ( roamingSettings.values["historyEventsCount"] == 1 ) {
-            $('section#history .item#no-events').remove();
+        var historyEventsCount = roamingSettings.values["historyEventsCount"];
+        historyEventsCount++;
+        if ( historyEventsCount > 0 ) {
+            $('section#history .item#no-events').fadeOut(250, function() {
+                $('section#history .item#no-events').remove();
+            });
         };
 
         // Add new Event to History
         var dt = new Date();
-        var date = dt.getDate() + "." + (dt.getMonth()+1) + " - " + dt.getHours() + ":" + dt.getMinutes();
+        var date = dt.getDate() + "." + (dt.getMonth()+1) + "  -  " + dt.getHours() + ":" + dt.getMinutes();
         var composite = new Windows.Storage.ApplicationDataCompositeValue();
         composite["date"] = date;
         composite["value"] = text;
-        roamingSettings.values[roamingSettings.values["historyEventsCount"]] = composite;
+        roamingSettings.values[historyEventsCount] = composite;
 
-        // Update Min value
-        if ( roamingSettings.values["historyEventsCount"] > max ) {
-            roamingSettings.values["historyEventsMin"] = roamingSettings.values["historyEventsMin"] + 1
+        // Update historyEventsMin
+        if ( historyEventsCount > maxHistoryEvents ) {
+            var historyEventsMin = roamingSettings.values["historyEventsMin"];
+            historyEventsMin++;
+
             $('section#history .item:last-child').hide();
             if (licenseInformation.productLicenses["1"].isActive) {
                 // show Message that limit has been reached ...
@@ -42,14 +47,13 @@ function trackHistory() {
         };
 
         // Add new Event to View
-        var i = roamingSettings.values["historyEventsCount"];
-        $('section#history').prepend('<div class="item" id=' + i + '><p class="time">' + composite["date"] + '</p><p class="large">' + composite["value"] + '</p></div>');
+        $('section#history').prepend('<div class="item" id=' + historyEventsCount + '><p class="time">' + composite["date"] + '</p><p class="large">' + composite["value"] + '</p></div>').css('opacity', 0).slideDown('slow').animate({ opacity: 1 }, { queue: false, duration: 'slow' });
 
-        composite = $('section#history .item#' + i + ' p.large');
-        if ( composite.html().length > 300 ) {
-            var text = composite.text();
+        item = $('section#history .item#' + historyEventsCount + ' p.large');
+        if ( item.html().length > 300 ) {
+            var text = item.text();
             text = text.substr(0,300) + '...';
-            composite.text(text);
+            item.text(text);
         };
     };
 };
