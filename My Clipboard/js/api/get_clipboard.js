f72485ec1805d12d952ec5f23bb17caba47072ba
jonhue <jonas.huebotter@gmx.de>
2017-02-26 15:45:57 +0100

    Version 2.1.1.0

--- My Clipboard/js/api/get_clipboard.js
+++ My Clipboard/js/api/get_clipboard.js
@@ -1,8 +1,9 @@
 ﻿function getClipboard() {
     var content = Windows.ApplicationModel.DataTransfer.Clipboard.getContent();
     if ( content.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text) ) {
-        var text = JSON.stringify(content.getTextAsync(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text));
-        $('section#show-clipboard #textarea').text(text);
+        var text = content.getTextAsync().then(function(result){
+            $('section#show-clipboard #textarea').text(text);
+        });
     } else {
         $('section#show-clipboard #textarea').text('Unable to show clipboard content');
     };
