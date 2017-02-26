088b6609b1a58854d8418da37940eb956654a871
jonhue <jonas.huebotter@gmx.de>
2017-02-26 15:50:33 +0100

    Fix Show Clipboard functionality

--- My Clipboard/js/api/get_clipboard.js
+++ My Clipboard/js/api/get_clipboard.js
@@ -1,8 +1,8 @@
 ﻿function getClipboard() {
     var content = Windows.ApplicationModel.DataTransfer.Clipboard.getContent();
     if ( content.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text) ) {
-        var text = content.getTextAsync().then(function(result){
-            $('section#show-clipboard #textarea').text(text);
+        content.getTextAsync().done(function(result){
+            $('section#show-clipboard #textarea').text(result);
         });
     } else {
         $('section#show-clipboard #textarea').text('Unable to show clipboard content');
